1a6fb4fa30280e49ab6461334d8921da3dc2fb71
Andreas Zeidler <witsch@plone.org>
2008-12-15 11:04:38 +0000

    simplify the dummy object to what's actually needed
    svn path=/plone.folder/tags/1.0a1/; revision=23793

--- plone/folder/tests/utils.py
+++ plone/folder/tests/utils.py
@@ -1,26 +1,10 @@
-from OFS.CopySupport import CopySource
 
 
-class DummyObject(CopySource):
+class DummyObject(object):
 
     def __init__(self, id, meta_type):
         self.id = id
         self.meta_type = meta_type
-
-    def cb_isMoveable(self):
-        return 1
-
-    def manage_afterAdd(self, item, container):
-        return
-
-    def manage_beforeDelete(self, item, container):
-        return
-
-    manage_afterAdd.__five_method__ = True
-    manage_beforeDelete.__five_method__ = True
-
-    def wl_isLocked(self):
-        return 0
 
     def __of__(self, obj):
         return self
