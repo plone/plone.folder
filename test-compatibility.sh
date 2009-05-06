1d426b614d502b0102c46ea4caae5a32b48d2440
Andreas Zeidler <witsch@plone.org>
2009-05-06 16:26:40 +0000

    move the remaining changes from the [http://plone.org/products/plone/roadmap/191 PLIP191]-related branches:   * [http://dev.plone.org/archetypes/changeset/11085/Products.Archetypes/branches/plip191-unified-folders?old=11085&old_path=Products.Archetypes%2Fbranches%2F1.5 Products.Archetypes/branches/plip191-unified-folders]   * [http://dev.plone.org/collective/changeset/85550/Products.ATContentTypes/branches/plip191-unified-folders?old=85550&old_path=Products.ATContentTypes%2Fbranches%2F1.2 Products.ATContentTypes/branches/plip191-unified-folders]   * [http://dev.plone.org/plone/changeset/26762/Plone/branches/plip191-unified-folders?old=26762&old_path=Plone%2Fbranches%2F3.2 Plone/branches/plip191-unified-folders] into a set of patches, which can be applied when running compatibility tests...
    svn path=/plone.folder/trunk/; revision=26764

--- test-compatibility.sh
+++ test-compatibility.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd $( dirname $0 )
+cat test-patches/* | patch -Nsp0
 bin/instance test --nowarn -s Products.CMFPlone
 bin/instance test --nowarn -s Products.ATContentTypes
 bin/instance test --nowarn -s Products.Archetypes
+cat test-patches/* | patch -Rsp0
