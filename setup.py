9e81d6d8244227281ade9713c029d2637ee2aae5
Hanno Schlichting <hanno@hannosch.eu>
2010-07-16 13:45:02 +0000

    Update license to GPL version 2 only.
    svn path=/plone.folder/trunk/; revision=37891

--- setup.py
+++ setup.py
@@ -1,43 +1,40 @@
 from setuptools import setup, find_packages
 
 version = '1.0'
-readme = open('README.txt').read()
-history = open('CHANGES.txt').read()
 
-setup(name = 'plone.folder',
-      version = version,
-      description = 'BTree-based folder implementation with order support',
-      long_description = readme[readme.find('\n\n'):] + '\n' + history,
-      classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Plone',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      keywords = 'folder btree order',
-      author = 'Plone Foundation',
-      author_email = 'plone-developers@lists.sourceforge.net',
-      url = 'http://pypi.python.org/pypi/plone.folder/',
-      license = 'LGPL',
-      packages = find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages = ['plone'],
-      include_package_data = True,
-      platforms = 'Any',
-      zip_safe = False,
-      install_requires = [
+setup(name='plone.folder',
+      version=version,
+      description='BTree-based folder implementation with order support',
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          "Intended Audience :: System Administrators",
+          "Intended Audience :: Other Audience",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='folder btree order',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.folder',
+      license='GPL version 2',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['plone'],
+      include_package_data=True,
+      platforms='Any',
+      zip_safe=False,
+      install_requires=[
           'setuptools',
           'plone.memoize',
       ],
-      extras_require = { 'test': [
+      extras_require={'test': [
           'profilehooks',
           'Products.CMFCore',
       ]},
-      entry_points = '',
 )
