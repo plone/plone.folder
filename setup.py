ba99db564bbc0200f7009c9c16e59f6894f7277a
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-15 14:46:15 +0200

    feat: declare dependencies

--- setup.py
+++ setup.py
@@ -41,19 +41,13 @@ setup(
     zip_safe=False,
     python_requires=">=3.8",
     install_requires=[
-        "plone.memoize",
-        "Products.BTreeFolder2",
         "Products.CMFCore",
         "Products.ZCatalog",
         "setuptools",
-        "zope.annotation",
-        "zope.component",
-        "zope.container",
-        "zope.interface",
-        "Zope2",
     ],
     extras_require={
         "test": [
+            "plone.app.testing",
             "profilehooks",
         ]
     },
