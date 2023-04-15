4ab86311378d1697bd7338feaa840d97db973090
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-15 14:34:54 +0200

    feat: pyroma

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     include_package_data=True,
     platforms="Any",
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "plone.memoize",
         "Products.BTreeFolder2",
