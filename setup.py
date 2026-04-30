9dcd3a5c9ba5d1351721e257f3178631fa6e65e5
Maurits van Rees <maurits@vanrees.org>
2026-04-30 22:07:07 +0200

    Restore Python 3.9 support.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
@@ -46,7 +47,7 @@ setup(
     include_package_data=True,
     platforms="Any",
     zip_safe=False,
-    python_requires=">=3.10",
+    python_requires=">=3.9",
     install_requires=[
         "setuptools",
         "Products.CMFCore",
